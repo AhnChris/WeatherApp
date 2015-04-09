484d51de0a15f0b0ed77e0adb8fdbfaa603e5053
Christopher Ahn <ahn.chris1@gmail.com>
2015-04-09 06:46:31 -0700

    Added new feature, GPS

---
+++ app/src/main/java/com/chrisahn/weatherapp/ui/CurrentLocationForecast.java
@@ -0,0 +1,290 @@
+package com.chrisahn.weatherapp.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chrisahn.weatherapp.R;
+import com.chrisahn.weatherapp.locations.LocationProvider;
+import com.chrisahn.weatherapp.weather.Current;
+import com.chrisahn.weatherapp.weather.Day;
+import com.chrisahn.weatherapp.weather.Forecast;
+import com.chrisahn.weatherapp.weather.Hour;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class CurrentLocationForecast extends ActionBarActivity implements
+        LocationProvider.LocationCallback {
+
+    private static final String TAG = CurrentLocationForecast.class.getSimpleName();
+
+    @InjectView(R.id.timeLabel) TextView mTimeLabel;
+    @InjectView(R.id.temperatureLabel) TextView mTemperatureLabel;
+    @InjectView(R.id.humidityValue) TextView mHumidityValue;
+    @InjectView(R.id.precipValue) TextView mPrecipValue;
+    @InjectView(R.id.summaryLabel) TextView mSummaryLabel;
+    @InjectView(R.id.iconImageView) ImageView mIconImageView;
+    @InjectView(R.id.refreshImageView) ImageView mRefreshImageView;
+    @InjectView(R.id.progressBar) ProgressBar mProgressBar;
+
+    private double mLatitude;
+    private double mLongitude;
+
+    private Forecast mForecast;
+
+    private LocationProvider mLocationProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_location_forecast);
+        ButterKnife.inject(this);
+        mLocationProvider = new LocationProvider(this, this);
+
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+/*        if (isGpsOn()) {
+            Toast.makeText(this, mLatitude + " " + mLongitude, Toast.LENGTH_LONG).show();
+            getLocationForecast(mLatitude, mLongitude);
+        }
+        else {
+            Toast.makeText(this, "GPS is disabled. Please enable GPS", Toast.LENGTH_LONG).show();
+        }
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocationForecast(mLatitude, mLongitude);
+            }
+        }); */
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationProvider.connect();
+    }
+
+    // Stop the location updates when the activity is not in focus
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
+
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        mLatitude  = location.getLatitude();
+        mLongitude = location.getLongitude();
+
+        if (isGpsOn()) {
+            Toast.makeText(this, mLatitude + " " + mLongitude, Toast.LENGTH_LONG).show();
+            getLocationForecast(mLatitude, mLongitude);
+        }
+        else {
+            Toast.makeText(this, "GPS is disabled. Please enable GPS", Toast.LENGTH_LONG).show();
+        }
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocationForecast(mLatitude, mLongitude);
+            }
+        });
+        //LatLng latLng = new LatLng(mLatitude, mLongitude);
+    }
+
+    public void getLocationForecast(double latitude, double longitude) {
+        String ApiKey = "f4a73f20558d9eba16d1a065c44a5b4d";
+        String url = "https://api.forecast.io/forecast/" + ApiKey +"/" + latitude
+                + "," + longitude;
+
+        // Requesting url
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call response = client.newCall(request);
+        // Call in background
+        response.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        toggleRefresh();
+                    }
+                }); alertUserError();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //toggleRefresh();
+                    }
+                });
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                    if (response.isSuccessful()) {
+                        mForecast = parseForecastData(jsonData);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateDisplay();
+                            }
+                        });
+
+
+                    } else {
+                        alertUserError();
+                    }
+                }
+                catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+                catch (JSONException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+
+            }
+        });
+
+    }
+
+    private void updateDisplay() {
+        Current current = mForecast.getCurrent();
+        mTemperatureLabel.setText(current.getTemperature() + "");
+        mHumidityValue.setText(current.getHumidity() + "");
+        mPrecipValue.setText(current.getPrecipChance() + "");
+        mSummaryLabel.setText(current.getSummary());
+        mTimeLabel.setText("At " + current.getFormattedTime() + " it will be");
+
+        Drawable drawable = getResources().getDrawable(current.getIconId());
+        mIconImageView.setImageDrawable(drawable);
+
+    }
+
+    private Forecast parseForecastData(String jsonData) throws JSONException {
+        Forecast forecast = new Forecast();
+
+        forecast.setCurrent(getCurrentDetails(jsonData));
+        forecast.setDailyForecast(getDailyDetails(jsonData));
+        forecast.setHourlyForecast(getHourDetails(jsonData));
+
+        return forecast;
+    }
+
+    private Current getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        Current current = new Current();
+        current.setHumidity(currently.getDouble("humidity"));
+        current.setIcon(currently.getString("icon"));
+        current.setPrecipChance(currently.getDouble("precipProbability"));
+        current.setSummary(currently.getString("summary"));
+        current.setTemperature(currently.getDouble("temperature"));
+        current.setTime(currently.getLong("time"));
+        current.setTimeZone(timezone);
+
+        return current;
+    }
+
+    private Day[] getDailyDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject daily = forecast.getJSONObject("daily");
+        JSONArray data = daily.getJSONArray("data");
+        Day[] days = new Day[data.length()];
+
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject jsonDay = data.getJSONObject(i);
+            Day day = new Day();
+            day.setTime(jsonDay.getLong("time"));
+            day.setSummary(jsonDay.getString("summary"));
+            day.setIcon(jsonDay.getString("icon"));
+            day.setTemperatureMax(jsonDay.getDouble("temperatureMax"));
+            day.setTimezone(timezone);
+
+            days[i] = day;
+        }
+
+        return days;
+    }
+
+    private Hour[] getHourDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject hourly = forecast.getJSONObject("hourly");
+        JSONArray data = hourly.getJSONArray("data");
+        Hour[] hours = new Hour[data.length()];
+
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject jsonHour = data.getJSONObject(i);
+            Hour hour = new Hour();
+            hour.setTimezone(timezone);
+            hour.setTime(jsonHour.getLong("time"));
+            hour.setIcon(jsonHour.getString("icon"));
+            hour.setSummary(jsonHour.getString("summary"));
+            hour.setTemperature(jsonHour.getDouble("temperature"));
+            hours[i] = hour;
+        }
+
+        return hours;
+    }
+
+    // Check to see if GPS is enabled
+    public boolean isGpsOn() {
+        boolean gpsAvail = false;
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (gpsStatus) {
+            gpsAvail = true;
+        }
+        return gpsAvail;
+    }
+
+    private void toggleRefresh() {
+        if (mProgressBar.getVisibility() == View.INVISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        }
+        else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void alertUserError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(),"error_dialog");
+    }
+}
